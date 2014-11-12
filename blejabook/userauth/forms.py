a39934d83cc703875873824fbf445d1b82c93344
Vladimir <Fejsov87@gmail.com>
2014-11-13 00:35:29 +0100

    falsifikat

--- blejabook/userauth/forms.py
+++ blejabook/userauth/forms.py
@@ -7,16 +7,23 @@ from django.forms.extras.widgets import SelectDateWidget
 from django.contrib.auth.forms import UserCreationForm
 from django.utils.translation import ugettext_lazy as _
 
-class MyUserForm(UserCreationForm):
-
-	def __init__(self, *args, **kw):
-		super(UserCreationForm, self).__init__(*args, **kw)
+class MyUserForm(forms.ModelForm):
+	"""
+    A form that creates a user, with no privileges, from the given username and
+    password.
+    """
+	def __init__(self, *args, **kwargs):
 		self.fields.keyOrder = [
 			'username',
 			'email',
 			'password1',
 			'password2',
 		]
+
+	error_messages = {
+        'duplicate_username': _("A user with that username already exists."),
+        'password_mismatch': _("The two password fields didn't match."),
+    }
 
 	username = forms.RegexField(label=_('Username'), regex=r'^\w+$', max_length=30, widget=forms.TextInput(attrs={'placeholder': 'Username', 'autofocus': 'autofocus'}), error_messages={ 'invalid': _('This value must contain only letters, numbers and underscores.') })	
 	email = forms.EmailField(label=_('Email'), required=True, widget=forms.TextInput(attrs={'placeholder': 'Email', 'autofocus': 'autofocus'}))
@@ -26,6 +33,44 @@ class MyUserForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ('username', 'email', 'password1', 'password2')
+
+	def clean_username(self):
+        # Since User.username is unique, this check is redundant,
+        # but it sets a nicer error message than the ORM. See #13147.
+        username = self.cleaned_data["username"]
+        try:
+            User._default_manager.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError(
+            self.error_messages['duplicate_username'],
+            code='duplicate_username',
+        )
+
+ 	def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
+        return password2
+
+ 	def clean_email(self):
+ 		email = self.cleaned_data['email']
+
+ 	def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+
+
+
 
 	def save(self, commit=True):
 		user = super(UserCreationForm, self).save(commit=False)
