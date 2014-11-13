4355f00c88e28b29545d74e0486e5ddd88e90402
Vladimir <Fejsov87@gmail.com>
2014-11-13 02:22:42 +0100

    view

--- blejabook/userauth/forms.py
+++ blejabook/userauth/forms.py
@@ -12,14 +12,7 @@ class MyUserForm(forms.ModelForm):
     A form that creates a user, with no privileges, from the given username and
     password.
     """
-	def __init__(self, *args, **kwargs):
-		self.fields.keyOrder = [
-			'username',
-			'email',
-			'password1',
-			'password2',
-		]
-
+	
 	error_messages = {
         'duplicate_username': _("A user with that username already exists."),
         'password_mismatch': _("The two password fields didn't match."),
@@ -35,53 +28,38 @@ class MyUserForm(forms.ModelForm):
 		fields = ('username', 'email', 'password1', 'password2')
 
 	def clean_username(self):
-        # Since User.username is unique, this check is redundant,
-        # but it sets a nicer error message than the ORM. See #13147.
-        username = self.cleaned_data["username"]
-        try:
-            User._default_manager.get(username=username)
-        except User.DoesNotExist:
-            return username
-        raise forms.ValidationError(
-            self.error_messages['duplicate_username'],
-            code='duplicate_username',
-        )
+		# Since User.username is unique, this check is redundant,
+		# but it sets a nicer error message than the ORM. See #13147.
+		username = self.cleaned_data["username"]
+		try:
+			User._default_manager.get(username=username)
+		except User.DoesNotExist:
+			return username
+		raise forms.ValidationError(
+			self.error_messages['duplicate_username'],
+			code='duplicate_username'
+		)
 
- 	def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError(
-                self.error_messages['password_mismatch'],
-                code='password_mismatch',
-            )
-        return password2
+	def clean_password2(self):
+		password1 = self.cleaned_data.get("password1")
+		password2 = self.cleaned_data.get("password2")
+		if password1 and password2 and password1 != password2:
+			raise forms.ValidationError(
+				self.error_messages['password_mismatch'],
+				code='password_mismatch',
+			)
+		return password2
 
- 	def clean_email(self):
+	def clean_email(self):
  		email = self.cleaned_data['email']
+ 		return email
 
  	def save(self, commit=True):
-        user = super(UserCreationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
-
-
-
-
-
-
-	def save(self, commit=True):
-		user = super(UserCreationForm, self).save(commit=False)
-		user.username = self.cleaned_data['username']
-		user.email = self.cleaned_data['email']
-		user.password1 = self.cleaned_data['password1']
-		user.password2 = self.cleaned_data['password2']
-
-		if commit:
-			user.save()
-		return user
+ 		user = super(MyUserForm, self).save(commit=False)
+ 		user.set_password(self.cleaned_data["password1"])
+ 		if commit:
+ 			user.save()
+ 		return user
 
 class UserProfileForm(forms.ModelForm):
 
@@ -99,7 +77,8 @@ class UserProfileForm(forms.ModelForm):
 		model = UserProfile
 		fields = ('gender', 'date_of_birth', 'country', 'city')
 
-    
+	
+   
 
-
+   
     
--- blejabook/userauth/views.py
+++ blejabook/userauth/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from userauth.forms import MyUserForm, UserProfileForm
-from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 
 def signup(request):
-	if request.method == 'POST':
-		pass
+	
+	if request.method=='POST':
+		user_form=MyUserForm(data=request.POST)
+		user_profile_form=UserProfileForm(data=request.POST)
+
+		if user_form.is_valid() and user_profile_form.is_valid():
+			user = user_form.save()
+			profile=user_profile_form.save(commit=False)
+			profile.user=user
+			profile.save()
+		else:
+			print user_form.errors, user_profile_form.errors
 
 	else:
 		user_form = MyUserForm()
 		user_profile_form = UserProfileForm()
 
-	return render(request, 'account/signup.html', {'user_form': user_form, 'user_profile_form': user_profile_form })
+	return render(request, 'account/signup.html', {'user_form':user_form, 'user_profile_form': user_profile_form})
+
+
+
+	
