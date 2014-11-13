275386be5afc97717b85c8bbaac1feb611ead2a1
Vladimir <Fejsov87@gmail.com>
2014-11-13 22:10:16 +0100

    login

--- blejabook/userauth/views.py
+++ blejabook/userauth/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from userauth.forms import MyUserForm, UserProfileForm
-
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib import auth
 
 # Create your views here.
 
@@ -22,7 +23,31 @@ def signup(request):
 		user_form = MyUserForm()
 		user_profile_form = UserProfileForm()
 
-	return render(request, 'account/signup.html', {'user_form':user_form, 'user_profile_form': user_profile_form})
+	return render(request, '/account/signup.html', {'user_form':user_form, 'user_profile_form': user_profile_form})
+
+def user_login(request):
+	if request.method=='POST':
+		username = request.POST['username']
+		password = request.POST['password']
+
+		user=auth.authenticate(username=username, password=password)
+		if user:
+			if user.is_active:
+				auth.login(request, user)
+				return HttpResponseRedirect('/admin/')
+			else:
+				return HttpResponse('jok')
+		else:
+			print 'invalid login details: {0}, {1}'.format(username, password)
+			return HttpResponse('invalid login details supplied')
+	else:
+		return render(request,'account/login.html/',{})
+
+
+
+
+
+    
 
 
 
